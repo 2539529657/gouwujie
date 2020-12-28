0bdb28554a244de51fcd97a42b91050d2a514a8d
lai <52686485+2539529657@users.noreply.github.com>
2020-12-28 16:10:54 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://192.168.124.61:8000',
+    baseURL: 'http://192.168.xx.xx:8000',
     timeout: 5000
   })
   instance.interceptors.request.use(config => {
